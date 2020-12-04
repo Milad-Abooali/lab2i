8c305b8445be9d1081e28c7e1c989565a5be6217
Milad.A <m.abooali@hotmail.com>
2020-12-04 17:20:56 +0300

    Header & Footer

---
+++ core/view/termeh/global/footer.php
@@ -0,0 +1,18 @@
+<div id="modal" class="modal fade mt-5" tabindex="-1" role="dialog">
+    <div class="modal-dialog mt-5" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"> </h5>
+                <button type="button" class="close close-right" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body></html>
