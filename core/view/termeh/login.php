0d695ea7aa3bc9df76f7f8e5bc17251a923676aa
Milad.A <m.abooali@hotmail.com>
2020-12-04 17:32:51 +0300

    Assets

--- core/view/termeh/login.php
+++ core/view/termeh/login.php
@@ -1,3 +1,23 @@
+<?php
+
+
+    $this->data['PAGE']['demo']=0;
+
+    $this->data['PAGE']['title'] = 'Login';
+    $this->data['PAGE']['keywords'] = 'test';
+    $this->data['PAGE']['description'] = 'test';
+    $this->data['PAGE']['robots'] = 1; // Null = Follow
+    $this->data['PAGE']['image'] = 'test';
+    $this->data['PAGE']['canonical'] = APP_URL;
+    $this->data['PAGE']['path'] = $this->page_path;
+    $this->data['PAGE']['amphtml'] = NULL;
+    $this->data['PAGE']['feed'] = NULL;
+
+    $this->data['PAGE']['head'] = ' ';
+
+    include_once $this->PATH."global/header.php";
+
+?>
 
 <div class="container">
     <div class="row justify-content-center">
@@ -63,3 +83,6 @@
         </div>
     </div>
 </div>
+
+
+<?php include_once $this->PATH."global/footer.php"; ?>
