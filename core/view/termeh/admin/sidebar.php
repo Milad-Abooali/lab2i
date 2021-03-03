d56b7451564a56a8d4eba4eb28a9cc88b177046b
Milad.A <m.abooali@hotmail.com>
2021-03-03 17:13:56 +0300

    Dashboard - Vendor

--- core/view/termeh/admin/sidebar.php
+++ core/view/termeh/admin/sidebar.php
@@ -1,7 +1,13 @@
+<?php
+/**
+ * @ TODO Admin page session
+ */
+
+?>
 <div class="accordion" id="adminMenu">
     <div class="menu-group">
         <div class="menu-title" id="headingOne">
-            <div class="mb-0 py-2 border-bottom" data-toggle="collapse" data-target="#a0">
+            <div class="mb-0 py-2" data-toggle="collapse" data-target="#a0">
                 <i class="fa fa-arrow-right"></i> Site Settings
             </div>
         </div>
@@ -13,35 +19,46 @@
     </div>
     <div class="menu-group">
         <div class="menu-title" id="headingTwo">
-            <div class="mb-0 py-2 border-bottom" data-toggle="collapse" data-target="#a1">
+            <div class="mb-0 py-2" data-toggle="collapse" data-target="#a1">
                 <i class="fa fa-arrow-right"></i>  Vendors
             </div>
         </div>
         <div id="a1" class="collapse" aria-labelledby="headingTwo" data-parent="#adminMenu">
             <a class="d-block py-2 pl-4 border-bottom" href="#">List Vendors</a>
-            <a class="d-block py-2 pl-4 border-bottom" href="#">Categories</a>
+            <a class="d-block py-2 pl-4 border-bottom" href="#">Verification</a>
+            <a class="d-block py-2 pl-4 border-bottom" href="#">Reviews</a>
         </div>
     </div>
     <div class="menu-group">
         <div class="menu-title" id="headingThree">
-            <div class="mb-0 py-2 border-bottom" data-toggle="collapse" data-target="#a2">
+            <div class="mb-0 py-2" data-toggle="collapse" data-target="#a2">
                 <i class="fa fa-arrow-right"></i> Users
             </div>
         </div>
         <div id="a2" class="collapse" aria-labelledby="headingThree" data-parent="#adminMenu">
             <a class="d-block py-2 pl-4 border-bottom" href="#">List Users</a>
-            <a class="d-block py-2 pl-4 border-bottom" href="#">Categories</a>
         </div>
     </div>
     <div class="menu-group">
         <div class="menu-title" id="headingThree">
-            <div class="mb-0 py-2 border-bottom" data-toggle="collapse" data-target="#a3">
+            <div class="mb-0 py-2" data-toggle="collapse" data-target="#a3">
                 <i class="fa fa-arrow-right"></i> Tenders & offers
             </div>
         </div>
         <div id="a3" class="collapse" aria-labelledby="headingThree" data-parent="#adminMenu">
             <a class="d-block py-2 pl-4 border-bottom" href="#">Active Tenders</a>
             <a class="d-block py-2 pl-4 border-bottom" href="#">Archive</a>
+        </div>
+    </div>
+    <div class="menu-group">
+        <div class="menu-title" id="headingThree">
+            <div class="mb-0 py-2" data-toggle="collapse" data-target="#a4">
+                <i class="fa fa-arrow-right"></i> Financial
+            </div>
+        </div>
+        <div id="a4" class="collapse" aria-labelledby="headingThree" data-parent="#adminMenu">
+            <a class="d-block py-2 pl-4 border-bottom" href="#">Invoices</a>
+            <a class="d-block py-2 pl-4 border-bottom" href="#">Transactions</a>
         </div>
     </div>
 </div>
