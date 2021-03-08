8ea313d5cf8b51d9ecf21535692a6988f2cf0a2c
Milad.A <m.abooali@hotmail.com>
2021-03-08 16:58:30 +0300

    Products

--- core/inc/vendor/new-product.php
+++ core/inc/vendor/new-product.php
@@ -16,3 +16,56 @@
     // Tags
     $tags = $db->selectAll('tags');
     foreach ($tags as $tag) $this->data['tags'][$tag['id']] = $tag;
+
+
+    if ($_POST ?? false) {
+        $insert['title'] = $_POST['category'];
+        $insert['excerpt'] = base64_encode($_POST['excerpt']);
+        $insert['category'] = $categories_id;
+        $insert['tags'] = implode(',',$_POST['tags']);
+
+        $this->data['insert_id'] = $db->insert('products', $insert);
+
+
+        if($this->data['insert_id']) {
+
+            // Update Tag Counter
+            if($_POST['tags'] ?? false) {
+                foreach($_POST['tags'] as $tag) {
+                    $where = "id=$tag";
+                    $db->increase('tags','count_p', $where);
+                }
+            }
+
+            // Upload Feature Image
+            if($_FILES['image'] ?? false) {
+                $count_image = count($_FILES['image']['name']);
+                for($i=0;$i<$count_image;$i++) {
+                    $fileNameCmps = explode(",", $_FILES['image']['name'][$i]);
+                    $filename = $i.strtolower(end($fileNameCmps));
+                    mkdir(APP_ROOT.'/cdn/upload/products/'.$this->data['insert_id']);
+                    $location = APP_ROOT.'/cdn/upload/products/'.$this->data['insert_id'].'/'.$filename;
+                    $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
+                    $valid_extensions = array("jpg","jpeg","png");
+                    if (in_array(strtolower($imageFileType),$valid_extensions)) if (move_uploaded_file($_FILES['image']['tmp_name'][$i],$location)) continue;
+                }
+            }
+
+            // Upload Feature Video
+            if($_FILES['video'] ?? false) {
+                $count_image = count($_FILES['video']['name']);
+                for($i=0;$i<$count_image;$i++) {
+                    $fileNameCmps = explode(",", $_FILES['video']['name'][$i]);
+                    $filename = $i.strtolower(end($fileNameCmps));
+                    mkdir(APP_ROOT.'/cdn/upload/products/'.$this->data['insert_id']);
+                    $location = APP_ROOT.'/cdn/upload/products/'.$this->data['insert_id'].'/'.$filename;
+                    $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
+                    $valid_extensions = array("jpg","jpeg","png");
+                    if (in_array(strtolower($imageFileType),$valid_extensions)) if (move_uploaded_file($_FILES['video']['tmp_name'][$i],$location)) continue;
+                }
+            }
+
+        }
+
+
+    }
