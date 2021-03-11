ce153fc31a26c1d04c9f8594af6b30b7e025cda9
Milad.A <m.abooali@hotmail.com>
2021-03-11 13:08:11 +0300

    Req

--- core/inc/user/add-request.php
+++ core/inc/user/add-request.php
@@ -11,17 +11,6 @@ use App\Core\M;
     // Database connection
     $db = new iSQL(DB_INFO);
 
-    // Products
-    $this->data['products'] = $db->selectAll('products');
-
-    // Categories
-    $categories = $db->selectAll('categories');
-    foreach ($categories as $category) $this->data['categories'][$category['id']] = $category;
-
     // Tags
     $tags = $db->selectAll('tags');
     foreach ($tags as $tag) $this->data['tags'][$tag['id']] = $tag;
-
-    // shops
-    $vendor_shop = $db->selectAll('vendor_shop');
-    foreach ($vendor_shop as $shop) $this->data['vendor_shop'][$shop['id']] = $shop;
